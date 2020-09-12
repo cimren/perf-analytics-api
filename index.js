25f3cb50e666a37b0910073b9d6cea498de3fb11
Cihan Imren <cihan.imren@gmail.com>
2020-09-12 18:52:59 +0000

    Updated cross origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(helmet())
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://cihan-perf-analytics-library.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
