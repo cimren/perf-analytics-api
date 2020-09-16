8446f40c10276613fc44a84166a0675572bbf0bd
Cihan Imren <cihan.imren@gmail.com>
2020-09-16 22:10:33 +0300

    Removed unnecessary code

--- index.js
+++ index.js
@@ -45,10 +45,6 @@ const getMetricsByUrl = (url, response) => {
 const addMetric = (request, response) => {
   const {url, TTFB,  FCP, domLoad, windowLoad} = request.body
   const dateTime = Date.now();
-
-  /* if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
-    return response.status(401).json({status: 'error', message: 'Unauthorized.'})
-  } */
   
   pool.query(
     'INSERT INTO perf_metrics (url, TTFB, FCP, domLoad, windowLoad, dateTime) VALUES ($1, $2, $3, $4, $5, $6)',
